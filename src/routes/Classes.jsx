10914d9acbe27d73c58ecc0dadc2f599082ecb19
david seol <dseol@davids-air-2.lan>
2026-07-04 23:05:31 -0400

    Update Engineering Design Process class to use images.jpeg

--- src/routes/Classes.jsx
+++ src/routes/Classes.jsx
@@ -9,6 +9,7 @@ import researcherAtWork from '../assets/skec/AdobeStock_171882033.jpeg'
 import geneticsImage from '../assets/skec/IMG_0495.JPG'
 import elevatingLearning from '../assets/skec/elevating_learning_stem_projects_for_high_school_students.webp.jpeg'
 import acellus from '../assets/skec/Acellus-AP-Biology.jpg'
+import engineeringTools from '../assets/images.jpeg'
 
 export default function Classes() {
   return (
@@ -263,7 +264,7 @@ export default function Classes() {
             </div>
           </div>
           <div className={styles.serviceCard}>
-            <img className={styles.serviceImg} src={elevatingLearning} alt="Engineering Design Process challenge" />
+            <img className={styles.serviceImg} src={engineeringTools} alt="Engineering Design Process challenge" />
             <div className={styles.serviceBody}>
               <h3>Engineering Design Process</h3>
               <div className={styles.divider} />
