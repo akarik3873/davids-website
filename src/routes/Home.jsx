bf87191de999a95d1a576668c91aace5f6657ecc
david seol <dseol@davids-air-2.lan>
2026-07-07 15:19:08 -0400

    Add Register Here button below FREE CLASS WEEK schedule on home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -106,6 +106,11 @@ export default function Home() {
             Register Here
           </a>
         </div>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '24px' }}>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSe2W7oZR381m2yMYk5QTfKyGmm0wWKougezfApOemPGVkIb0Q/viewform?usp=publish-editor" target="_blank" rel="noreferrer noopener" className={styles.bookBtn}>
+            Register Here
+          </a>
+        </div>
       </section>
 
       <section className={`${styles.section} ${styles.sectionAlt}`}>
